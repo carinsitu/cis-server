8d0930e7f31703bdf5784c2d43b8127300af6fe2
Romuald Conty <romuald@opus-codium.fr>
2019-05-18 03:33:05 +0200

    Move Car class into dedicated file

--- lib/carmaster.rb
+++ lib/carmaster.rb
@@ -1,52 +1,12 @@
 require 'carmaster/version'
-require 'socket'
+require 'carmaster/car'
+
 require 'sdl2'
 
 require 'byebug'
 
 module Carmaster
   class Error < StandardError; end
-
-  class Car
-    def initialize(ip, port, joystick)
-      @ip = ip
-      @port = port
-      @joystick = SDL2::Joystick.open(joystick)
-      @udp_socket = UDPSocket.new
-
-      @joystick_last_forward = 0
-      @joystick_last_backward = 0
-    end
-
-    def send(data)
-      @udp_socket.send(data, 0, @ip, @port)
-    end
-
-    def process_joystick_event(event)
-      case event.axis
-      when 0
-        self.steering = event.value
-      when 2
-        @joystick_last_backward = -(event.value + 32_768) / 2
-        self.throttle = @joystick_last_forward.zero? ? @joystick_last_backward : 0
-      when 5
-        @joystick_last_forward = (event.value + 32_768) / 2
-        self.throttle = @joystick_last_backward.zero? ? @joystick_last_forward : 0
-      end
-    end
-
-    def steering=(value)
-      puts "#steering= #{value}"
-      data = [0x10, value].pack('cs')
-      send data
-    end
-
-    def throttle=(value)
-      puts "#throttle= #{value}"
-      data = [0x11, value].pack('cs')
-      send data
-    end
-  end
 
   class Master
     def initialize
