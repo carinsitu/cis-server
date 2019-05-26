6f4c784a3cbb0ed78e0b16909a0b58b8180701c0
Gauthier Legrand <gauthier@imaleo.io>
2019-05-26 22:28:18 +0200

    Add mqtt client singleton

---
+++ lib/carmaster/mqttclient.rb
@@ -0,0 +1,14 @@
+require 'mqtt'
+require 'singleton'
+
+module CarMaster
+    class MqttClient
+        include Singleton
+
+        attr_reader :mqtt
+
+        def connect 
+            @mqtt = MQTT::Client.connect('localhost')
+        end
+    end
+end
