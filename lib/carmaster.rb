cedb59ebc987fb6768d8cfe4b08756ba2e1ef033
Romuald Conty <romuald@opus-codium.fr>
2019-05-26 23:54:09 +0200

    Move GameController event handling code into dedicated class

--- lib/carmaster.rb
+++ lib/carmaster.rb
@@ -1,48 +1,29 @@
 require 'carmaster/version'
 
 require 'carmaster/carsmaster'
-require 'carmaster/gamecontroller'
-
-require 'sdl2'
+require 'carmaster/gamecontrollersmaster'
 
 require 'byebug'
+
+require 'singleton'
 
 module CarMaster
   class Error < StandardError; end
 
   class Master
+    include Singleton
+
     def initialize
-      SDL2.init(SDL2::INIT_JOYSTICK)
-
-      @game_controllers = {}
-
       @cars_master = CarsMaster.new
       @cars_master.request_discovery_on_lan
+
+      @game_controllers_master = GameControllersMaster.new
     end
 
-    def run # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength
+    def run
       loop do
         @cars_master.handle_udp_socket
-
-        while (event = SDL2::Event.poll)
-          case event
-          when SDL2::Event::JoyAxisMotion, SDL2::Event::JoyButton
-            p event
-            @game_controllers[event.which].process_event event
-          when SDL2::Event::JoyDeviceAdded
-            p event
-            game_controller = GameController.new(event.which)
-            @game_controllers[event.which] = game_controller
-            game_controller.car = @cars_master.cars.first.last
-          when SDL2::Event::JoyDeviceRemoved
-            p event
-            @game_controllers[event.which] = nil
-          when SDL2::Event::KeyDown
-            exit if event.scancode == SDL2::Key::Scan::ESCAPE
-          when SDL2::Event::Quit
-            exit
-          end
-        end
+        @game_controllers_master.process_sdl_events
 
         sleep 0.001
       end
---
+++ lib/carmaster/gamecontrollersmaster.rb
@@ -0,0 +1,42 @@
+require 'carmaster/gamecontroller'
+
+require 'sdl2'
+
+module CarMaster
+  class GameControllersMaster
+    attr_reader :game_controllers
+
+    def initialize
+      SDL2.init(SDL2::INIT_JOYSTICK)
+
+      @game_controllers = {}
+
+      @sdl_next_instance_id = 0
+    end
+
+    def register(sdl_device_id)
+      game_controller = GameController.new(sdl_device_id)
+      @game_controllers[@sdl_next_instance_id] = game_controller
+      puts "Game controller registered: device_id=#{sdl_device_id} instance_id=#{@sdl_next_instance_id}"
+      @sdl_next_instance_id += 1
+    end
+
+    def unregister(instance_id)
+      puts "Game controller unregistered: instance_id=#{instance_id}"
+      @game_controllers.delete_if { |id, _game_controller| instance_id == id }
+    end
+
+    def process_sdl_events
+      while (event = SDL2::Event.poll)
+        case event
+        when SDL2::Event::JoyAxisMotion, SDL2::Event::JoyButton
+          @game_controllers[event.which].process_event event
+        when SDL2::Event::JoyDeviceAdded
+          register event.which
+        when SDL2::Event::JoyDeviceRemoved
+          unregister event.which
+        end
+      end
+    end
+  end
+end
