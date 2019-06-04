e43caec06159d5c90adf23e17c634153f18a482a
Manuel Tancoigne <m.tancoigne@gmail.com>
2019-06-04 10:39:25 +0200

    Rename "Car.send()" to "Car.send_data()"
    We shouold not override "send()"

--- lib/carmaster/car.rb
+++ lib/carmaster/car.rb
@@ -15,14 +15,14 @@ module CarMaster
       puts "Car(#{@ip}) instanciated"
     end
 
-    def send(data)
+    def send_data(data)
       @udp_socket.send(data, 0, @ip, 4210)
     end
 
     def steering=(value)
       puts "#steering= #{value}"
       data = [0x10, value].pack('cs')
-      send data
+      send_data data
     end
 
     def throttle=(value)
@@ -30,14 +30,14 @@ module CarMaster
       value *= @throttle_factor
       puts "#throttle= #{value}"
       data = [0x11, value].pack('cs')
-      send data
+      send_data data
     end
 
     def trim_steering(value)
       puts "#trim_steering= #{@trim_steering}"
       @trim_steering += value
       data = [0x20, @trim_steering].pack('cc')
-      send data
+      send_data data
       self.steering = 0
     end
 
